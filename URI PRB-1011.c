57ae966f60112b42ec7596c147d4f2173b2845f9
ardipta <ashiqur35-2149@diu.edu.bd>
2019-05-28 05:39:29 +0600

    URI problem 1012

--- URI PRB-1011.c
+++ URI PRB-1011.c
@@ -1,10 +1,20 @@
 #include<stdio.h>
 
 int main(){
-    double pi=3.14159, R, volume;
-    scanf("%lf", &R);
-    volume = 4/3.0 * pi * (R*R*R);
-    printf("VOLUME = %.3f\n", volume);
+     float A, B, C, triangle, circle,trapezium, square, rectangle;
+     scanf("%f %f %f", &A, &B, &C);
+     triangle = (A*C)/2;
+     circle = 3.14159*(C*C);
+     trapezium = ((A+B)*C)/2;
+     square = B*B;
+     rectangle = A*B;
+     printf("TRIANGULO: %.3f\n", triangle);
+     printf("CIRCULO: %.3f\n", circle);
+     printf("TRAPEZIO: %.3f\n", trapezium);
+     printf("QUADRADO: %.3f\n", square);
+     printf("RETANGULO: %.3f\n", rectangle);
+
+
 
     return 0;
 
