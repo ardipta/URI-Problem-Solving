e2f7fc07783a3c8301fafab1a3221638af7833e7
ardipta <ashiqur35-2149@diu.edu.bd>
2019-05-29 22:13:34 +0600

    URI problem 1016

---
+++ URI PRB-1016.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int distance, time;
+    scanf("%d", &distance);
+    time = distance * 2;
+    printf("%d minutos\n", time);
+
+    return 0;
+}
